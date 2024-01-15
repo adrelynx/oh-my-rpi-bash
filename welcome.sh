5ec0d1793867c7b7c76ebe497f4c5578d132b67e
adrelynx <adrelynx@gmail.com>
2024-01-15 06:58:07 +0300

    initial commit

--- welcome.sh
+++ welcome.sh
@@ -3,11 +3,28 @@
 hostName=`uname -n`
 diskSpace=`df -Ph | grep /dev/root | awk '{print $4}' | tr -d '\n'`
 memoryUsed=`free -t -m | grep Total | awk '{print $3" MB";}'`
+momoryFree=`free -t -m | grep Total | awk '{print $4" MB";}'`
+
+function getIPAddress() {
+    local ip_route
+    ip_route=$(ip -4 route get 8.8.8.8 2>/dev/null)
+    if [[ -z "$ip_route" ]]; then
+        ip_route=$(ip -6 route get 2001:4860:4860::8888 2>/dev/null)
+    fi
+    [[ -n "$ip_route" ]] && grep -oP "src \K[^\s]+" <<< "$ip_route"
+}
+getPublicIP=`curl -s https://icanhazip.com`
+
 
 echo "
 ===========================================
- - Hostname............: $hostName
- - Disk Space..........: $diskSpace
- - Memory used.........: $memoryUsed
+- User@Hostname.......: $USER@$hostName
+- Disk Space..........: $diskSpace
+- Memory used.........: $memoryUsed
+- Memory free.........: $momoryFree
+- Local IP............: $(getIPAddress)
+- Public IP...........: $getPublicIP
+- Shell...............: $SHELL
 ===========================================
 "
+
